e5f578cc0650290899033ce9d780f2f481a441d3
Cole Gleason <colewashere@gmail.com>
2012-03-08 05:31:34 -0600

    moved tray to bottom and made it gray

--- examples/advanced/lightSequencer/Sequencer.java
+++ examples/advanced/lightSequencer/Sequencer.java
@@ -3,6 +3,7 @@ package advanced.lightSequencer;
 import org.mt4j.components.MTComponent;
 
 import processing.core.PApplet;
+import processing.core.PGraphics;
 
 public class Sequencer extends MTComponent{
 	PlayHead p;
@@ -15,6 +16,11 @@ public class Sequencer extends MTComponent{
 		this.p = new PlayHead(applet);
 		this.lights = new LightManager(applet, p, lights);
 		this.lights.start();
+	}
+	
+	public void drawComponent(PGraphics g) {
+		g.fill(100);
+		g.rect(0, 9*applet.height/10, applet.width, applet.height/10 + 1);
 	}
 	
 
